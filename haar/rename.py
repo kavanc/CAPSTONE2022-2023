f979eab0302c5e4f7d198505262c39d6cb4069b8
Nate Grobe <natepgrobe@gmail.com>
2022-11-20 12:55:48 -0500

    posture and classifier merged

--- haar/rename.py
+++
@@ -1,8 +0,0 @@
-import os
-
-folder = f"C:/Users/natep/Documents/GitHub/CAPSTONE2022-2023/haar/temp/n/"
-
-for i, fn in enumerate(os.listdir(folder)):
-    src = f"{folder}/{fn}"
-    dest = f"C:/Users/natep/Documents/GitHub/CAPSTONE2022-2023/haar/temp/n/{i+1}.jpg"
-    os.rename(src, dest)
