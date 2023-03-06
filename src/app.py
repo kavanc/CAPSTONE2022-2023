9401247cbdbb2eef8d2e52d1156663bbca601534
Nate Grobe <natepgrobe@gmail.com>
2023-03-06 16:03:28 -0500

    added logging

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 from tkinter import *
 from PIL import Image, ImageTk
+from datetime import datetime
 
 # ultralytics
 from ultralytics import YOLO
@@ -16,6 +17,7 @@ from image_utils import draw_framerate, save_image
 '''
     TODO: 
     - clean up layout
+    - add settings menu instead of checkboxes
 '''
 
 class App:
@@ -25,50 +27,52 @@ class App:
         self.window.title("Suspicious Activity Detection")
         self.vid_stopped = True
 
+        self.header_font = "helvetica 12 bold"
 
-        # for now this is the display size
-        self.canvas = Canvas(window, width=960, height=720)
-        self.canvas.pack()
+        # video frame
+        self.vid_frame = Frame(window)
+        self.vid_frame.grid(row=0, column=0)
+        # video dispay
+        self.canvas = Canvas(self.vid_frame, width=960, height=720)
+        self.canvas.grid(row=0, column=0)
+        # start stop button
+        self.start_btn = Button(self.vid_frame, text='Start', height=2, width=10, command=self.handle_start_stop)
+        self.start_btn.grid(row=1, column=0)
+
+        # logging frame
+        self.log_frame = Frame(window)
+        self.log_frame.grid(row=0, column=1)
+        self.log_label = Label(self.log_frame, text="Logs", font=self.header_font)
+        self.log_label.grid(row=0, column=0)
+        self.log_box = Text(self.log_frame, height=45, width=30, cursor=None)
+        self.log_box.configure(state='disabled')
+        self.log_box.grid(row=1, column=0)
+
+        # options frame
+        self.options_frame = Frame(window)
+        self.options_frame.grid(row=0, column=2)
+        # options header
+        self.options_label = Label(self.options_frame, text="Options", font=self.header_font)
+        self.options_label.grid(row=0, column=0)
+        # input checkbox
+        self.webcam_cb_state = False
+        self.webcam_cb = Checkbutton(self.options_frame, text="Webcam", variable=self.webcam_cb_state, onvalue=True, offvalue=False, command=self.toggle_webcam_cb)
+        self.webcam_cb.grid(row=1, column=0)
+        # screenshot checkbox
+        self.sh_cb_state = True
+        self.sh_cb = Checkbutton(self.options_frame, text="Screenshot", variable=self.sh_cb_state, onvalue=True, offvalue=False, command=self.toggle_sh_cb)
+        self.sh_cb.select()
+        self.sh_cb.grid(row=2, column=0)
 
 
         # models setup
         self.w_model = YOLO("models/knifeDetector.pt")
-        self.model_cb_state = True
-
-        # buttons
-        self.ss_button()
-
-        # checkboxes
-        # webcam checkbox
-        self.webcam_cb_state = False
-        self.webcam_checkbox()
-
-        # screenshot checkbox
-        self.sh_cb_state = True
-        self.sh_checkbox()
 
         self.window.mainloop()
-
-    # buttons
-    # start/stop button
-    def ss_button(self):
-        self.start_btn = Button(self.window, text='Start', height=2, width=10, command=self.handle_start_stop)
-        self.start_btn.pack()
-
-    # checkboxes
-    # toggles webcam or video
-    def webcam_checkbox(self):
-        self.webcam_cb = Checkbutton(self.window, text="Webcam", variable=self.webcam_cb_state, onvalue=True, offvalue=False, command=self.toggle_webcam_cb)
-        self.webcam_cb.pack()
 
     def toggle_webcam_cb(self):
         if self.vid_stopped:
             self.webcam_cb_state = not self.webcam_cb_state
-
-    def sh_checkbox(self):
-        self.sh_cb = Checkbutton(self.window, text="Screenshot", variable=self.sh_cb_state, onvalue=True, offvalue=False, command=self.toggle_sh_cb)
-        self.sh_cb.select()
-        self.sh_cb.pack()
 
     def toggle_sh_cb(self):
         self.sh_cb_state = not self.sh_cb_state
@@ -110,6 +114,10 @@ class App:
             self.last_sh = current_frame
             save_image(img)
 
+    def update_log_box(self, text):
+        self.log_box.configure(state='normal')
+        self.log_box.insert(END, f"{str(datetime.now())}\n{text}")
+        self.log_box.configure(state='disabled')
 
     def video_loop(self):
         ret, img = self.cap.read()
@@ -130,8 +138,16 @@ class App:
                     b = box.xyxy[0]
                     c = box.cls
 
-                    header = f"{self.w_model.names[int(c)]} {box.conf[0]:.2f}"
+                    confidence = box.conf[0]
+                    w_type = self.w_model.names[int(c)]
+                    header = f"{w_type} {confidence:.2f}"
                     annotator.box_label(b, header, (0, 0, 255))
+
+                    if confidence > 0.5 and confidence < 0.8:
+                        self.update_log_box(f"Potential {w_type} found.\n\n")
+
+                    if confidence > 0.8:
+                        self.update_log_box(f"Positive {w_type} found.\n\n")
 
                     # takes a screenshot with minimum 20 frame (~1s) separation
                     if self.sh_cb_state:
